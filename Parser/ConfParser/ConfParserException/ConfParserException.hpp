14136c2b847369b4063ee158b9a22d6bdb262cd0
sob2545 <sob2545@naver.com>
2023-11-30 23:29:24 +0900

    FEAT: add Conf parser exception class to handle exception entire of conf block

---
+++ Parser/ConfParser/ConfParserException/ConfParserException.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <exception>
+#include <string>
+#include "../../../utils/Color.hpp"
+
+namespace CONF {
+	enum E_POS {
+		POS = 0,
+		LINE,
+		COLUMN
+	};
+}
+
+class ConfParserException : public std::exception {
+private:
+	std::string		m_Message;
+	ConfParserException();
+
+public:
+	ConfParserException(const ConfParserException& other);
+	ConfParserException& operator=(const ConfParserException& other);
+	ConfParserException(const std::string& file, const std::string& error, const std::string& message, const size_t* pos);
+	virtual ~ConfParserException() throw();
+	virtual const char* what() const throw();
+	const std::string& getMessage() const;
+};
