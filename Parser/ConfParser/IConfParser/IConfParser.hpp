bcbd715031c4106b0712bf18a1dff32827b92b6f
sob2545 <sob2545@naver.com>
2023-12-02 10:38:13 +0900

    CHORE: delte useless header and function in interface class

--- Parser/ConfParser/IConfParser/IConfParser.hpp
+++ Parser/ConfParser/IConfParser/IConfParser.hpp
@@ -1,17 +1,16 @@
 #pragma once
 
 #include "../ConfParserException/ConfParserException.hpp"
-#include <cstddef>
 
 class IConfParser {
+private:
 public:
 	virtual ~IConfParser() {}
-	virtual void			contextLines(const std::string& file, size_t* index) = 0;
-	virtual void			argumentChecker(const std::vector<std::string>& args, const unsigned char& status, const size_t* index) = 0;
-	virtual void			directives(const std::string& file, size_t* index) = 0;
-	virtual bool			blockContent(const std::string& file, size_t* index) = 0;
-	virtual bool			context(const std::string& file, size_t* index) = 0;
-	virtual std::string		argument(const std::string& file, size_t* index, const unsigned char& status) = 0;
-	virtual unsigned char	directiveNameChecker(const std::string& name, size_t* index) = 0;
-	virtual unsigned char	directiveName(const std::string& file, size_t* index) = 0;
+	virtual void				argumentChecker(const std::vector<std::string>& args, const unsigned char& status, const size_t* index) = 0;
+	virtual void				directives(const std::string& file, size_t* index) = 0;
+	virtual const bool			contextLines(const std::string& file, size_t* index) = 0;
+	virtual const bool			context(const std::string& file, size_t* index) = 0;
+	virtual const std::string	argument(const std::string& file, size_t* index, const unsigned char& status) = 0;
+	virtual const unsigned char	directiveNameChecker(const std::string& name, size_t* index) = 0;
+	virtual const unsigned char	directiveName(const std::string& file, size_t* index) = 0;
 };
