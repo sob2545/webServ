9f96b99f37ad231ff2ec99a307bb3965375d6297
sob2545 <sob2545@naver.com>
2023-12-03 18:49:00 +0900

    FEAT: add location status comment

--- Parser/Conf/ConfData/ConfLocationBlock.hpp
+++ Parser/Conf/ConfData/ConfLocationBlock.hpp
@@ -4,6 +4,19 @@
 #include <map>
 #include <vector>
 
+/**
+ * @brief	Server Block Status
+ * @details unsigned short : 2 byte
+ *  
+ *	0b			 		 1 = root
+ *	0b  		 		10 = index
+ *	0b         		   100 = autoindex
+ *	0b         		  1000 = error_page
+ *	0b       		1 0000 = access_log
+ *	0b     		   10 0000 = internal
+ *	0b 1000 0000 0000 0000 = location
+ */
+
 namespace   CONF {
 	typedef	std::vector<std::string>	strVec;
 
