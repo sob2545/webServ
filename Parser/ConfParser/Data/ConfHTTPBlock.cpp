9c2238fd2aaee4c7e4f0a0270e9f01c95283dddb
sob2545 <sob2545@naver.com>
2023-12-10 23:49:13 +0900

    FEAT: add getter in HTTP block and working progress in parsing error page, server

--- Parser/ConfParser/Data/ConfHTTPBlock.cpp
+++ Parser/ConfParser/Data/ConfHTTPBlock.cpp
@@ -1,11 +1,17 @@
 #include "ConfHTTPBlock.hpp"
 #include "ConfServerBlock.hpp"
+#include <string>
+#include "../../MIMEParser/MIMEParser.hpp"
+
+// TODO: delete
+#include <iostream>
 #include <sys/_types/_size_t.h>
 
 CONF::HTTPBlock::HTTPBlock()
 : AConfParser(),
   m_Autoindex(false),
   m_Status(0),
+  m_KeepAliveTime(75),
   m_Default_type("text/plain") {
 	initHTTPStatusMap();
 }
@@ -25,8 +31,106 @@ void	CONF::HTTPBlock::initHTTPStatusMap() {
 }
 
 
-const bool	CONF::HTTPBlock::argumentChecker(const std::vector<std::string>& args, const unsigned short& status) {
+bool	CONF::HTTPBlock::argumentChecker(const std::vector<std::string>& args, const unsigned short& status) {
+	const std::string&	fileContent = CONF::ConfFile::getInstance()->getFileContent();
+	const size_t&		fileSize = CONF::ConfFile::getInstance()->getFileSize();
+	size_t*				Pos = CONF::ConfFile::getInstance()->Pos();
 
+	switch (status) {
+		case CONF::E_HTTP_BLOCK_STATUS::ROOT: {
+			if (args.size() != 1) {
+				throw ConfParserException(args.at(0), "invalid number of Root arguments!");
+			} else {
+				(args[0].empty()) ? throw ConfParserException(args.at(0), "invalid number of Root arguments!") : this->m_Root = args[0];
+			}
+			return false;
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::INDEX: {
+			for (size_t i = 0; i < args.size(); i++) {
+				(args[i].empty()) ? throw ConfParserException(args.at(0), "invalid number of Index arguments!") : this->m_Index.insert(args[i]);
+			}
+			return false;
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::AUTOINDEX: {
+			if (args.size() != 1) {
+				throw ConfParserException(args.at(0), "invalid number of Autoindex arguments!");
+			} else {
+				if (args.at(0).empty()) {
+					throw ConfParserException(args.at(0), "invalid number of Autoindex arguments!");
+				}
+				if (args.at(0) == "on") {
+					this->m_Autoindex = true;
+				} else if (args.at(0) == "off") {
+					this->m_Autoindex = false;
+				} else {
+					throw ConfParserException(args.at(0), "invalid number of Autoindex arguments!");
+				}
+			}
+			return false;
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::ERROR_PAGE: {
+			for (size_t i = 0; i < args.size() - 1; i++) {
+				if (args[i].empty()) {
+					throw ConfParserException(args[i], "invalid number of Error Page arguments!");
+				}
+
+				// TODO: check error page more detail (ex: goto location, replace status code)
+				char*	endptr;
+				const long	argumentNumber = std::strtol(args[i].c_str(), &endptr, 10);
+				if (*endptr != '\0' || argumentNumber < 100 || argumentNumber > 599) {
+					throw ConfParserException(args[i], "invalid number of Error Page arguments!");
+				}
+				this->m_Error_page.insert(std::make_pair(static_cast<unsigned int>(argumentNumber), ""));
+			}
+			return false;
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::ACCESS_LOG: {
+			if (args.size() != 1) {
+				throw ConfParserException(args.at(0), "invalid number of Access Log arguments!");
+			} else {
+				(args[0].empty()) ? throw ConfParserException(args[0], "invalid number of Access Log arguments!") : this->m_Access_log = args[0];
+			}
+			return false;
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::KEEPALIVE_TIMEOUT: {
+			if (args.size() != 1) {
+				throw ConfParserException(args.at(0), "invalid number of Keepalive Timeout arguments!");
+			} else {
+				const int sec = std::atoi(args[0].c_str());
+				(sec < 0) ? throw ConfParserException(args.at(0), "invalid number of Keepalive Timeout arguments!") : this->m_KeepAliveTime = sec;
+			}
+			return false;
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::INCLUDE: {
+			if (args.size() != 1) {
+				throw ConfParserException(args.at(0), "invalid number of Include arguments!");
+			} else {
+				(args[0].empty()) ? throw ConfParserException(args[0], "invalid number of Include arguments!") : 0;
+				std::cout << "include : " << args[0] << std::endl;
+				const size_t		dotPos = args[0].rfind('.');
+				const std::string&	extension = args[0].substr(dotPos + 1, args[0].size() - dotPos);
+				if (extension == "types") {
+					MIME::Parser(args[0], this->m_Mime_types);
+				}
+			}
+			return false;
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::DEFAULT_TYPE: {
+			if (args.size() != 1) {
+				throw ConfParserException(args.at(0), "invalid number of Default Type arguments!");
+			} else {
+				(args[0].empty()) ? throw ConfParserException(args[0], "invalid number of Default Type arguments!") : this->m_Default_type = args[0];
+			}
+			return false;
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::SERVER: {
+			if (args.size() > 1 || (args.size() == 1 && !args[0].empty())) {
+				throw ConfParserException(args.at(0), "invalid number of Server arguments!");
+			}
+			return true;
+		}
+	}
+	throw ConfParserException("", "Invalid configure file!");
 }
 
 const std::string	CONF::HTTPBlock::argument(const unsigned short& status) {
@@ -45,10 +149,26 @@ const std::string	CONF::HTTPBlock::argument(const unsigned short& status) {
 		case CONF::E_HTTP_BLOCK_STATUS::ACCESS_LOG:
 		case CONF::E_HTTP_BLOCK_STATUS::INDEX:
 		case CONF::E_HTTP_BLOCK_STATUS::INCLUDE:
-			stringPathArgumentParser(argument);
+			if (!stringPathArgumentParser(argument)) {
+				throw ConfParserException(argument, "invalid root argument format!");
+			}
 			return (argument);
+			// TODO: delete, is it work?
+			// return (stringPathArgumentParser(argument) ? argument : throw ConfParserException(argument, "invalid root argument format!"));
 		case CONF::E_HTTP_BLOCK_STATUS::ERROR_PAGE: {
 			errorPageArgumentParser(argument);
+			return (argument);
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::DEFAULT_TYPE: {
+			const size_t	startPos = Pos[E_INDEX::FILE];
+			argument = MIME::type(fileContent, Pos[E_INDEX::FILE]);
+			Pos[E_INDEX::COLUMN] += Pos[E_INDEX::FILE] - startPos;
+			return (argument);
+		}
+		case CONF::E_HTTP_BLOCK_STATUS::KEEPALIVE_TIMEOUT: {
+			if (!digitArgumentParser(argument)) {
+				throw ConfParserException(argument, "invalid number of Keepalive Timeout arguments!");
+			}
 			return (argument);
 		}
 	}
@@ -68,7 +188,7 @@ const std::string	CONF::HTTPBlock::argument(const unsigned short& status) {
 }
 
 
-const unsigned short	CONF::HTTPBlock::directiveNameChecker(const std::string& name) {
+unsigned short	CONF::HTTPBlock::directiveNameChecker(const std::string& name) {
 	const std::string&	fileContent = CONF::ConfFile::getInstance()->getFileContent();
 	size_t*				Pos = CONF::ConfFile::getInstance()->Pos();
 
@@ -82,7 +202,7 @@ const unsigned short	CONF::HTTPBlock::directiveNameChecker(const std::string& na
 	}
 }
 
-const bool	CONF::HTTPBlock::blockContent() {
+bool	CONF::HTTPBlock::blockContent() {
 	const std::string&	fileContent = CONF::ConfFile::getInstance()->getFileContent();
 	size_t*				Pos = CONF::ConfFile::getInstance()->Pos();
 
@@ -94,10 +214,10 @@ const bool	CONF::HTTPBlock::blockContent() {
 	Pos[E_INDEX::COLUMN]++;
 
 	// TODO: implement serverBlock
-	ServerBlock	serverBlock;
-	serverBlock.initialize();
+	// ServerBlock	serverBlock;
+	// serverBlock.initialize();
 
-	m_Server_block.insert(std::make_pair(serverBlock.getServerName(), serverBlock));
+	// m_Server_block.insert(std::make_pair(serverBlock.getServerName(), serverBlock));
 
 	if (fileContent[Pos[E_INDEX::FILE]] != E_CONF::RBRACE) {
 		throw ConfParserException("}", "Direct block has no brace!");
@@ -109,7 +229,7 @@ const bool	CONF::HTTPBlock::blockContent() {
 }
 
 
-const bool	CONF::HTTPBlock::context() {
+bool	CONF::HTTPBlock::context() {
 	const std::string&	fileContent = CONF::ConfFile::getInstance()->getFileContent();
 	const size_t&		fileSize = CONF::ConfFile::getInstance()->getFileSize();
 	size_t*				Pos = CONF::ConfFile::getInstance()->Pos();
@@ -123,7 +243,6 @@ const bool	CONF::HTTPBlock::context() {
 	} else {
 		return (ABNF::isC_nl(fileContent, Pos[E_INDEX::FILE]) ? true : false);
 	}
-
 }
 
 void	CONF::HTTPBlock::initialize() {
@@ -132,3 +251,34 @@ void	CONF::HTTPBlock::initialize() {
 	contextLines();
 }
 
+const bool&	CONF::HTTPBlock::getAutoindex() const {
+	return (this->m_Autoindex);
+}
+
+const std::string&	CONF::HTTPBlock::getRoot() const {
+	return (this->m_Root);
+}
+
+const std::string	CONF::HTTPBlock::getIndex(const std::string& uri) const {
+	return (this->m_Index.find(uri));
+}
+
+const std::string&	CONF::HTTPBlock::getAccess_log() const {
+	return (this->m_Access_log);
+}
+
+const unsigned int&	CONF::HTTPBlock::getKeepAliveTime() const {
+	return (this->m_KeepAliveTime);
+}
+
+const std::string&	CONF::HTTPBlock::getDefault_type() const {
+	return (this->m_Default_type);
+}
+
+const std::map<unsigned short, std::string>&	CONF::HTTPBlock::getError_page() const {
+	return (this->m_Error_page);
+}
+
+const CONF::HTTPBlock::TypeMap&	CONF::HTTPBlock::getMime_types() const {
+	return (this->m_Mime_types);
+}
--- Parser/ConfParser/Data/ConfHTTPBlock.hpp
+++ Parser/ConfParser/Data/ConfHTTPBlock.hpp
@@ -2,7 +2,7 @@
 
 #include "../Parser/AConfParser.hpp"
 #include "../../../Trie/Trie.hpp"
-#include "ConfServerBlock.hpp"
+// #include "ConfServerBlock.hpp"
 #include <map>
 #include <string>
 #include <vector>
@@ -27,32 +27,35 @@
 namespace   CONF {
 	class HTTPBlock : public AConfParser {
 	private:
-		typedef std::map<std::string, unsigned short>	statusMap;
+		typedef std::map<std::string, unsigned short>				statusMap;
+		typedef std::map<unsigned short, std::string>				errorPageMap;
+		typedef std::map<std::string, std::vector<std::string> >	TypeMap ;
 
-		bool								m_Autoindex;
-		unsigned short						m_Status;
-		std::string							m_Default_type;
-		std::string							m_Root;
-		std::string							m_Access_log;
-		std::string							m_Include;
-		std::map<unsigned int, std::string>	m_Error_page;
-		std::map<std::string, std::string>	m_Mime_types;
-		std::map<std::string, ServerBlock>	m_Server_block;
-		statusMap							m_HTTPStatusMap;
-		Trie								m_Index;
+		bool									m_Autoindex;
+		unsigned short							m_Status;
+		unsigned int							m_KeepAliveTime;
+		std::string								m_Default_type;
+		std::string								m_Root;
+		std::string								m_Access_log;
+		std::string								m_Include;
+		Trie									m_Index;
+		errorPageMap							m_Error_page;
+		TypeMap									m_Mime_types;
+		// std::map<std::string, ServerBlock>		m_Server_block;
+		statusMap								m_HTTPStatusMap;
 
 	private:
 		HTTPBlock(const HTTPBlock& other);
 		HTTPBlock& operator=(const HTTPBlock& other);
 
-		void					initHTTPStatusMap();
+		void				initHTTPStatusMap();
 
-		const bool				context();
-		const bool				blockContent();
-		const unsigned short	directiveNameChecker(const std::string& name);
+		bool				context();
+		bool				blockContent();
+		unsigned short		directiveNameChecker(const std::string& name);
 
-		const std::string		argument(const unsigned short& status);
-		const bool				argumentChecker(const std::vector<std::string>& args, const unsigned short& status);
+		const std::string	argument(const unsigned short& status);
+		bool				argumentChecker(const std::vector<std::string>& args, const unsigned short& status);
 	
 	public:
 		HTTPBlock();
@@ -60,5 +63,14 @@ namespace   CONF {
 
 		void					initialize();
 
+		const bool&				getAutoindex() const;
+		const unsigned int&		getKeepAliveTime() const;
+		const std::string&		getDefault_type() const;
+		const std::string&		getRoot() const;
+		const std::string&		getAccess_log() const;
+		const std::string&		getInclude() const;
+		const std::string		getIndex(const std::string& uri) const;
+		const errorPageMap&		getError_page() const;
+		const TypeMap&			getMime_types() const;
 	};
 }
