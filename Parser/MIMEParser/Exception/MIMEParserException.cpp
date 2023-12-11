a99a9c8f8dd4032b1d0bf1c880b6a32c2b2ce4d5
sesim <sesim@student.42seoul.kr>
2023-12-11 14:24:17 +0900

    FEAT: add MIME exception class

---
+++ Parser/MIMEParser/Exception/MIMEParserException.cpp
@@ -0,0 +1,39 @@
+#include "MIMEParserException.hpp"
+#include "../MIMEFile/MIMEFile.hpp"
+#include <sstream>
+#include <string>
+
+MIMEParserException::MIMEParserException() {}
+
+MIMEParserException::MIMEParserException(const std::string& error, const std::string& message) {
+	std::stringstream	res;
+	if (!MIME::MIMEFile::getInstance()) {
+		res << BOLDRED << "Error: " << BOLDWHITE "MIMEParserException: MIMEFile::getInstance() is NULL" << RESET;
+		m_Message = res.str();
+		return ;
+	}
+	const std::string&	file = MIME::MIMEFile::getInstance()->getFileName();
+	const size_t*		pos = MIME::MIMEFile::getInstance()->Pos();
+
+	res << BOLDWHITE << file << ":" << pos[1] << ":" << pos[2] << BOLDRED << " Error:" << BOLDWHITE << " \"" << error << "\" " << message << RESET;
+	m_Message = res.str();
+}
+
+MIMEParserException::MIMEParserException(const MIMEParserException& other) {
+	*this = other;
+}
+
+MIMEParserException& MIMEParserException::operator=(const MIMEParserException& other) {
+	m_Message = other.m_Message;
+	return *this;
+}
+
+MIMEParserException::~MIMEParserException() throw() {}
+
+const char* MIMEParserException::what() const throw() {
+	return m_Message.c_str();
+}
+
+const std::string& MIMEParserException::getMessage() const {
+	return m_Message;
+}
---
+++ Parser/MIMEParser/Exception/MIMEParserException.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <exception>
+#include <string>
+#include "../../../utils/Color.hpp"
+
+class MIMEParserException : public std::exception {
+private:
+	std::string		m_Message;
+	MIMEParserException();
+
+public:
+	MIMEParserException(const MIMEParserException& other);
+	MIMEParserException& operator=(const MIMEParserException& other);
+	MIMEParserException(const std::string& error, const std::string& message);
+	virtual ~MIMEParserException() throw();
+	virtual const char* what() const throw();
+	const std::string& getMessage() const;
+};
