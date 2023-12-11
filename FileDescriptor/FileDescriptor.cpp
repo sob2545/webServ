8dcf8d0c43ca402b04858f0bf0b1b27702154ba4
sesim <sesim@student.42seoul.kr>
2023-12-11 14:25:39 +0900

    CHORE: fix return type with const reference

--- FileDescriptor/FileDescriptor.cpp
+++ FileDescriptor/FileDescriptor.cpp
@@ -4,4 +4,4 @@ FileDescriptor::FileDescriptor(int fd) : m_Fd(fd) {}
 
 FileDescriptor::~FileDescriptor() { close(m_Fd); }
 
-const int FileDescriptor::getFd() const { return m_Fd; }
+const int& FileDescriptor::getFd() const { return m_Fd; }
