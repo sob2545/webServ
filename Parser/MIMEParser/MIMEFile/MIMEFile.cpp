8b7ea02d1f10284491fb16c942e2a32051bc969a
sesim <sesim@student.42seoul.kr>
2023-12-11 14:23:59 +0900

    FEAT: add MIME file singleton class

---
+++ Parser/MIMEParser/MIMEFile/MIMEFile.cpp
@@ -0,0 +1,31 @@
+#include "MIMEFile.hpp"
+
+MIME::MIMEFile*	MIME::MIMEFile::instance = NULL;
+
+void	MIME::MIMEFile::InitInstance(const std::string &fileName) {
+	if (!MIME::MIMEFile::instance) {
+		MIME::MIMEFile::instance = new MIME::MIMEFile(fileName);
+	}
+}
+
+MIME::MIMEFile*	MIME::MIMEFile::getInstance() {
+	return MIME::MIMEFile::instance;
+}
+
+MIME::MIMEFile::MIMEFile(const std::string& fileName): ReadFile(fileName) {
+	m_Pos[E_INDEX::FILE] = 0;
+	m_Pos[E_INDEX::LINE] = 1;
+	m_Pos[E_INDEX::COLUMN] = 1;
+}
+
+MIME::MIMEFile::~MIMEFile() {
+}
+
+void	MIME::MIMEFile::Destroy() {
+	delete MIME::MIMEFile::instance;
+	MIME::MIMEFile::instance = NULL;
+}
+
+size_t*	MIME::MIMEFile::Pos() {
+	return m_Pos;
+}
---
+++ Parser/MIMEParser/MIMEFile/MIMEFile.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../../../FileDescriptor/File/ReadFile.hpp"
+
+namespace E_INDEX {
+	enum E_INDEX {
+		FILE = 0,
+		LINE,
+		COLUMN
+	};
+}
+
+namespace MIME {
+	class MIMEFile : public ReadFile {
+	private:
+		size_t				m_Pos[3];
+		static MIMEFile*	instance;
+
+		MIMEFile(const MIMEFile& other);
+		MIMEFile& operator=(const MIMEFile& other);
+
+	public:
+		static void			InitInstance(const std::string& fileName);
+		static MIMEFile*	getInstance();
+
+		MIMEFile(const std::string& fileName);
+		virtual ~MIMEFile();
+
+		void	Destroy();
+		size_t*	Pos();
+	};
+}
