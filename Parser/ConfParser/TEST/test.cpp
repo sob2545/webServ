e2fac8e06e548777637c7f2bfc750d4fe7e0cb01
sesim <sesim@student.42seoul.kr>
2023-12-13 21:15:04 +0900

    FEAT: test for server block. Parse works well except location block

--- Parser/ConfParser/TEST/test.cpp
+++ Parser/ConfParser/TEST/test.cpp
@@ -1,8 +1,14 @@
 #include "../Data/ConfBlock.hpp"
+#include <cstdlib>
 #include <iostream>
 #include <unistd.h>
 
+// void	a() {
+// 	system("leaks a.out");
+// }
+
 int main(int ac, char** av, char** env) {
+	// atexit(a);
 	(void)ac;
 	(void)av;
 	system("clear");
@@ -11,8 +17,10 @@ int main(int ac, char** av, char** env) {
 	try {
 		CONF::ConfBlock::initInstance(fileName, env);
 		CONF::ConfBlock::getInstance()->print();
+		CONF::ConfBlock::getInstance()->destroy();
 	} catch (ConfParserException& e) {
 		std::cerr << e.getMessage() << std::endl;
+		CONF::ConfBlock::getInstance()->destroy();
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
