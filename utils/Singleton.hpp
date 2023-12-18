7b2d1b4ca363dfbb80de22ef4b0239fc540476cd
sesim <sesim@student.42seoul.kr>
2023-12-18 19:40:09 +0900

    FEAT: singleton class constructor overloading for take 2 parameters

--- utils/Singleton.hpp
+++ utils/Singleton.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 /**
  * @brief singleton
  * @details singleton을 상속 후, instance 함수를 직접호출하여 싱글톤관리를 할 수 있다.
@@ -15,5 +17,10 @@ public:
     static T& instance(void) {
 		static T _s_instance;
         return (_s_instance);
+    }
+
+    static T& instance(const std::string& fileName, char** env) {
+        static T _s_instance(fileName, env);
+		return (_s_instance);
     }
 };
