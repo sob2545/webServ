72d7221224d8f76afb1dc858eb6e5fd117431044
sesim <sesim@student.42seoul.kr>
2023-12-19 21:33:57 +0900

    FEAT: change temp buffer with unique_ptr, have to implement throw exception

--- FileDescriptor/File/ReadFile.cpp
+++ FileDescriptor/File/ReadFile.cpp
@@ -1,11 +1,14 @@
 #include "ReadFile.hpp"
 #include "fcntl.h"
 #include "sys/stat.h"
+#include "../../Utils/SmartPointer.hpp"
 
 
 ReadFile::ReadFile(const std::string& filename) : FileDescriptor(0), m_FileName(filename) {
 	this->m_Fd = open(filename.c_str(), O_RDONLY);
 
+	// TODO: Implement throw
+	(m_Fd < 0) ? throw  : 0;
 	struct stat	buf;
 	if (fstat(m_Fd, &buf) < 0) {
 		// TODO: exception class
@@ -14,16 +17,14 @@ ReadFile::ReadFile(const std::string& filename) : FileDescriptor(0), m_FileName(
 	m_FileSize = buf.st_size;
 
 	// read에 string을 매개변수로 넘기지 못한다.
-	char* tmp = new char[m_FileSize + 1];
+	ft::unique_ptr<char[]>	tmp(::new char[m_FileSize + 1]);
 	const ssize_t readSize = read(m_Fd, &tmp[0], m_FileSize);
 	if (readSize < 0 || m_FileSize != readSize) {
 		// TODO: exception class
-		delete [] tmp;
 		close(m_Fd);
 	}
 	tmp[readSize] = '\0';
-	m_FileContent.assign(tmp, readSize);
-	delete [] tmp;
+	m_FileContent.assign(tmp.get(), readSize);
 }
 
 ReadFile::~ReadFile() {}
--- FileDescriptor/File/ReadFile.hpp
+++ FileDescriptor/File/ReadFile.hpp
@@ -7,14 +7,18 @@
 class ReadFile : public FileDescriptor {
 protected:
 	const std::string	m_FileName;
-	std::size_t				m_FileSize;
+	std::size_t			m_FileSize;
 	std::string			m_FileContent;
 
-public:
-	const std::string&	getFileName() const;
-	const std::size_t&		getFileSize() const;
-	const std::string&	getFileContent() const;
+private:
+	ReadFile(const ReadFile& other);
+	ReadFile& operator=(const ReadFile& other);
 
+public:
 	ReadFile(const std::string& fileName);
-	~ReadFile();
+	virtual ~ReadFile();
+
+	const std::string&	getFileName() const;
+	const std::size_t&	getFileSize() const;
+	const std::string&	getFileContent() const;
 };
