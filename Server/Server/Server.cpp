867e9704e1d07d122b7a27ffe258fdbbb94dc0b0
sob2545 <sob2545@naver.com>
2023-12-21 12:13:49 +0900

    CHORE: delete comment

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -7,8 +7,6 @@ Server::Server(const CONF::ServerBlock& block)
  : m_Socket(block.getIP(), block.getPort())
 {
 	m_ServerBlock.push_back(block);
-	// 생성자에서 소켓을 바로 생성하면 안됨. 포트를 기준으로 생성할지 넘어갈지 결정해야함.
-	// 하나의 포트에 여러 개의 서버를 받을 수 있도록 하여, shared_ptr을 사용해 공유하도록 함.
 }
 
 Server::~Server() {}
