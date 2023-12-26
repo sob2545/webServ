97a161c42c9e00ba0d475c25b961a60451fabe13
sesim <sesim@student.42seoul.kr>
2023-12-26 19:36:52 +0900

    TEST: test for union
    When initalize union, only POD(Plain Old Data: like ) types can in C++98
    Initalize class or struct in union is since C++11

---
+++ practice/union_keyword/test.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+struct OriginFormType {
+    std::string m_Path;
+
+    // 기본 생성자
+    OriginFormType() {}
+
+    // 기본 소멸자
+    ~OriginFormType() {}
+};
+
+struct AbsoluteFormType {
+    unsigned short m_Port;
+    std::string m_Host;
+    std::string m_Path;
+
+    // 기본 생성자
+    AbsoluteFormType() : m_Port(0) {}
+
+    // 기본 소멸자
+    ~AbsoluteFormType() {}
+};
+
+struct ResponseRecipe {
+    typedef std::map<std::string, std::vector<std::string> > QueryMap;
+
+    bool isChunk;
+    unsigned char m_Method;
+    unsigned char m_Status;
+    unsigned char m_Version;
+    unsigned short m_Headers;
+    unsigned int m_ContentLength;
+
+    std::string m_Host;
+    std::string m_ContentType;
+
+    std::string m_RequestTarget;
+
+    std::vector<unsigned char> m_BodyMessage();
+    std::map<std::string, std::string> m_Cookie;
+    QueryMap m_Query;
+
+	union {
+        OriginFormType m_OriginData;
+        AbsoluteFormType m_AbsoluteForm;
+    };
+
+    ResponseRecipe(const std::string& test) {
+        m_OriginData.m_Path = test;
+    }
+    // 소멸자
+    ~ResponseRecipe() {}
+};
+
+int main() {
+    ResponseRecipe test("test");
+}
