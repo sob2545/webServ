0621565a11987c053085ba1546880a383ec635c2
sesim <sesim@student.42seoul.kr>
2024-02-02 14:39:58 +0900

    FEAT: add HTTP message exception class function

--- Parser/HTTPMessageParser/HTTPException/RequestURITooLongException.hpp
+++ Parser/HTTPMessageParser/HTTPException/RequestURITooLongException.hpp
@@ -6,6 +6,7 @@
 namespace HTTP {
 	class	RequestURITooLongException : std::exception {
 	private:
+		std::string		m_Message;
 
 	public:
 		RequestURITooLongException();
@@ -13,6 +14,7 @@ namespace HTTP {
 		RequestURITooLongException& operator=(const RequestURITooLongException& other);
 		virtual ~RequestURITooLongException() throw();
 
-		virtual const char*	what() const throw();
+		virtual const char* what() const throw();
+		const std::string& getMessage() const;
 	};
 }
